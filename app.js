4c082f5f29cefffe373f31a639bf5fbd69298c42
Nabeel Khan <nabeelkhan2004@hotmail.com>
2023-09-24 16:21:10 +0500

    Update Calculator

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-var userInput = document.getElementById("input")
+var userInput = document.getElementById("input");
 
 function getNumber(num) {
   userInput.value += num;
 }
 
 function getOperator(operator) {
-  
   var operatorArr = ["/", "+", "-", "%", "*", "."];
-  
+
   for (var i = 0; i < operatorArr.length; i++) {
     if (userInput.value[userInput.value.length - 1] === operatorArr[i]) {
-      userInput.value = userInput.value.slice(0, -1)
+      userInput.value = userInput.value.slice(0, -1);
     }
   }
   userInput.value += operator;
@@ -28,7 +27,11 @@ function deleteChar() {
 }
 
 function result() {
-  var result = document.getElementById("input");
-  result.value = eval(result.value).toFixed(5)
-  1
+  var result = eval(userInput.value).toString();
+  for(var i = 0; i < (result.toString()).length; i++){
+    if(result.charCodeAt(i) === 46){
+      result = Number(result).toFixed(3)
+    }
+  }
+  userInput.value = result
 }
